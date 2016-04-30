6f6ac4df8963cd7948d804a8986e60370fc83307
Shala Qweghen <serafettinyilmaz04@gmail.com>
2016-04-30 15:43:02 +0300

    Combine bubble_sort and bubble_sort_by together

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,3 +12,19 @@ def bubble_sort array
 end
 
 puts bubble_sort [16,17,6,7,80,37,39,65,47,91,89,21,2,6,89,2,6,99,4,87,63,33,44,55,66,22,21,45,78,8]
+
+####################################################################################################
+
+def bubble_sort_by (array, &block)
+	a = 0
+	(array.size*2).times do
+		a == (array.size - 1) ? a = 0 : a
+		if block.call(array[a], array[a+1]) < 0
+			array[a], array[a+1] = array[a+1], array[a]
+		end
+		a += 1
+	end
+	array.inspect
+end
+
+puts bubble_sort_by(["hi","helllo","they", "bye","Mikey", "top", "a"]) {|left, right| left.length - right.length}
